374a5d2dade686f65be4392851ead18836826af5
Hardik <hardik8784@gmail.com>
2021-10-03 09:42:17 -0400

    Updated GDD and the Instruction screen to match with the Scale of the phone.

--- Assets/[Scripts]/UIBehaviour.cs
+++ Assets/[Scripts]/UIBehaviour.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Author’s name - Hardik Dipakbhai Shah 
+/// Student Number - 101249099
+/// Date last Modified - 10/03/2021
+///description and Revision History
+/// This code is for changing the scenes 
+/// </summary>
+
 public class UIBehaviour : MonoBehaviour
 {
     private int nextSceneIndex;
@@ -10,8 +18,8 @@ public class UIBehaviour : MonoBehaviour
 
     void Start()
     {
-        nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-        previousSceneIndex = SceneManager.GetActiveScene().buildIndex - 1;
+        nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;              // This code will be use to change to next scene
+        previousSceneIndex = SceneManager.GetActiveScene().buildIndex - 1;          // This code will be used to change to previous scene
     }
 
     public void OnNextButtonPressed()
@@ -23,7 +31,7 @@ public class UIBehaviour : MonoBehaviour
         SceneManager.LoadScene(previousSceneIndex);
     }
 
-    public void OnMainMenuButtonPressed()
+    public void OnMainMenuButtonPressed()                                           // This code is to change the scene from GameOverScreen to MenuScreen
     {
         SceneManager.LoadScene("MenuScreen");
     }
