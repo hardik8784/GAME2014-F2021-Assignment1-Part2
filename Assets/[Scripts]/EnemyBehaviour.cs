3c7bb40e597fe3e07c76481a51e80b8eadfec78e
Hardik <hardik8784@gmail.com>
2021-10-21 14:00:54 -0400

    Added EnemyBehaviour

---
+++ Assets/[Scripts]/EnemyBehaviour.cs
@@ -0,0 +1,46 @@
+/*
+ * Full Name: Hardik Dipakbhai Shah
+ * Student ID : 101249099
+ * Date Modified : October 19,2021
+ * File : EnemyController.cs
+ * Description : This is the Enemy Controller Script
+ * Revision History : v0.1 > Added Comments to know the Code better before start anything & to include a program header
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float horizontalSpeed;
+    public float horizontalBoundary;
+    public float direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    private void _Move()
+    {
+        transform.position += new Vector3(horizontalSpeed * direction * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    private void _CheckBounds()
+    {
+        // check right boundary
+        if (transform.position.x >= horizontalBoundary)
+        {
+            direction = -1.0f;
+        }
+
+        // check left boundary
+        if (transform.position.x <= -horizontalBoundary)
+        {
+            direction = 1.0f;
+        }
+    }
+}
